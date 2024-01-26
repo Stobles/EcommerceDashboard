a822f0c993d4f360f390507ce0aeb6f8a31ec909
Stobles <ilovespnjensen@inbox.ru>
2024-01-26 23:38:43 +0300

    fix: billboard add form validator

--- lib/validators/billboard.ts
+++ lib/validators/billboard.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const BillboardValidator = z.object({
-  imageUrl: z.string().min(2, "Строка должна содержать 2 и более символов."),
+  imageUrl: z.string().min(1, "Билборд должен содержать изображение."),
   label: z.string().min(2, "Строка должна содержать 2 и более символов."),
 });
 
